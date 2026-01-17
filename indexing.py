73766190c4765949a537eacf169b70de9e856629
vanatuuu <lucanamenslos@gmail.com>
2026-01-17 11:19:24 +0100

    i gues irgendein falscher index stuf

--- indexing.py
+++ indexing.py
@@ -69,7 +69,7 @@ data = pd.read_csv(file)
 # islice(..., 10) beschränkt auf die ersten 10 Einträge – bei Bedarf anpassen/entfernen
 
 #for index, row in islice data.iterrows(): # für alle zeilen (kann nen bissl dauern)
-for index, row in data[:10].iterrows():
+for index, row in data[:].iterrows():
     # Neues Tantivy-Dokument
     doc = Document()
 
